b60514a257df803b71d28f0a96ff975700cb97af
Gibran Gonzalez <hector.gibran@hotmail.com>
2020-10-01 15:45:03 -0500

    Create room joining view

---
+++ munchkinonline/src/views/Play.vue
@@ -0,0 +1,63 @@
+<template>
+    <div id="play">
+        <Card v-bind:cardData="cardData" v-on:btn-click="createGame"/>
+    </div>
+</template>
+
+<script>
+import Card from '../components/site-interface/Card'
+
+export default {
+    name: 'play',
+    components: {
+        Card
+    },
+    data () {
+        return {
+            cardData: {
+                title: "Create or Join Game",
+                textFields: [
+                    {
+                        id: 100,
+                        placeholder: "Room Name"
+                    }
+                ],
+                buttons: [
+                    {
+                        id: 201,
+                        buttonText: "Create Game"
+                    },
+                    {
+                        id: 202,
+                        buttonText: "Join Game"
+                    },
+                ],
+                footerLink : {
+                    display: false,
+                    route: "",
+                    text: ""
+                }
+            }
+        }
+    },
+    methods: {
+        //data = ["roomName"]
+        createGame(data) {
+            // Game creation here
+            console.log(data);
+        }
+    }
+}
+</script>
+
+<style scoped>
+#play {
+    display: flex;
+    justify-content: center;
+}
+
+.elem {
+    padding-top: 10px;
+}
+
+</style>
