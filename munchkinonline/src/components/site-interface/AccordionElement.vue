06d326c26bdc60dd864d14e935e8b10e135d5567
Gibran Gonzalez <hector.gibran@hotmail.com>
2020-10-05 16:14:14 -0500

    Adds Accordion Element (unfinished)

---
+++ munchkinonline/src/components/site-interface/AccordionElement.vue
@@ -0,0 +1,33 @@
+<template>
+    <b-card no-body class="mb-1">
+        <b-card-header header-tag="header" class="p-1" role="tab">
+            <b-button v-b-toggle.accordion-1 class="button" variant="info">Accordion 1</b-button>
+        </b-card-header>
+        <b-collapse id="accordion-1" accordion="game-history" role="tabpanel">
+            <b-card-body>
+                TEXT
+            </b-card-body>
+        </b-collapse>
+    </b-card>
+</template>
+
+<script>
+import {BCard, BButton, BCollapse} from 'bootstrap-vue'
+
+export default {
+    name: 'AccordionElement',
+    components: {
+        BCard,
+        BButton,
+        BCollapse
+    }
+}
+</script>
+
+<style scoped>
+
+.button {
+    width: 100%;
+}
+
+</style>
--- munchkinonline/src/views/Profile.vue
+++ munchkinonline/src/views/Profile.vue
@@ -1,33 +1,35 @@
 <template>
   <div class = 'container'>
     <div>
-      <b-card class="profileCard" id="profile" title="Your Profile">
+      <b-card id="profile" title="Your Profile">
         <b-card-text>Username: {{userData.name}}</b-card-text>
         <b-card-text>Email: {{userData.email}}</b-card-text>
         <b-card-text>Date Joined: {{userData.joined}}</b-card-text>
       </b-card>
     </div>
 
-    <b-card bg-variant = 'dark' text-variant = 'white' title = 'Historial de Juego'>
-      <b-card-text>
-        <p></p>
-        <p></p>
-      </b-card-text>
-      <b-button href="#" variant="primary" @click = 'logOut()'>Cerrar sesión</b-button>
-    </b-card>
+    <div>
+      <b-card id="history" title='Game History'>
+        <div class="accordion" role="tablist">
+          <AccordionElement />
+        </div>
+      </b-card>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import {BCard} from 'bootstrap-vue';
-import firebase from 'firebase'; 
+import firebase from 'firebase';
+import AccordionElement from '../components/site-interface/AccordionElement.vue'
 import { userCollection } from '../main.js';
 
 export default {
   name: "profile",
   components: {
-    BCard
+    BCard,
+    AccordionElement
   },
   data() {
     return {
@@ -69,18 +71,17 @@ export default {
 </script>
 
 <style scoped>
-
 .container {
   text-align: left;
 }
 
-.card {
+#history {
   width: 70%;
   margin: 0 auto;
 }
 
 #profile {
-  width: 50%;
+  width: 60%;
   margin: 20px auto 50px auto;
 }
 
