6f9d6fce661dbf60ccb9bfa1255077fae1c5f769
Gibran Gonzalez <hector.gibran@hotmail.com>
2020-10-05 19:55:00 -0500

    Adds player standings and room names to game history

--- munchkinonline/src/components/site-interface/AccordionElement.vue
+++ munchkinonline/src/components/site-interface/AccordionElement.vue
@@ -1,11 +1,13 @@
 <template>
     <b-card no-body class="mb-1">
         <b-card-header header-tag="header" class="p-1" role="tab">
-            <b-button v-b-toggle="entryData.id" class="button" variant="info">{{entryData.data.date}}</b-button>
+            <b-button v-b-toggle="entryData.id" class="button" variant="info">{{entryData.data.room}} - {{entryData.data.date}}</b-button>
         </b-card-header>
         <b-collapse v-bind:id="entryData.id" accordion="game-history" role="tabpanel">
             <b-card-body>
-                {{entryData.data.usernames}}
+                <p v-bind:key="index" v-for="(element, index) in elements">
+                    <span v-bind:class="element.pos">{{element.pos}}.  {{element.value}}</span>
+                </p>
             </b-card-body>
         </b-collapse>
     </b-card>
@@ -21,7 +23,29 @@ export default {
         BButton,
         BCollapse
     },
-    props: ["entryData"]
+    data() {
+        return {
+            elements: []
+        }
+    },
+    created() {
+        this.generatePositions()
+    },
+    props: ["entryData"],
+    methods: {
+        generatePositions() {
+            var usernames = this.entryData.data.usernames
+            var winners = this.entryData.data.winners
+            console.log(usernames, winners)
+
+            usernames.forEach((username, index) => {
+                if (winners.includes(username))
+                    this.elements.push({value: username, pos: "1"})
+                else
+                    this.elements.push({value: username, pos:`${index+1}`})
+            })
+        }
+    }
 }
 </script>
 
