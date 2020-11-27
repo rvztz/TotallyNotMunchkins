c3cc8543976ef802aebbb9ab27f1073b586adbe1
Gibran Gonzalez <hector.gibran@hotmail.com>
2020-11-26 18:37:12 -0600

    Fills About page

--- munchkinonline/src/views/About.vue
+++ munchkinonline/src/views/About.vue
@@ -1,21 +1,36 @@
 <template>
   <div class="about">
     <div class="contain">
-    <h1 id="title">What is Munchkin?</h1>
-    <div>
-     <img src="https://cdn.influenster.com/media/product/image/78007287f452384fa0b13378f2229ef4.png" alt="Test image" class="gif"/>
-    </div>
-	<p class="bio">
-	</p>
-	<p class="bio">
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at orci vel turpis viverra blandit. 
-  Etiam quis pretium nibh. Donec et urna orci. Mauris rhoncus consequat quam at dignissim. Cras in libero nisi. Curabitur in placerat neque.
-	</p>
-	<p class="bio">
-  Morbi a varius dui, non eleifend neque. Phasellus luctus magna nunc, ut malesuada est fermentum vel. 
-  Fusce vehicula neque at nulla fringilla, in blandit lacus ornare. Nunc ornare ex sit amet tortor lobortis mollis. Fusce placerat dolor et eros vestibulum, sed luctus enim elementum.
-  </p>
+      <div>
+      <img src="https://www.bestchoicereviews.org/wp-content/uploads/2014/12/munchkin-card-and-board-games.png" alt="Test image" class="gif"/>
       </div>
+      <div class="aboutText">
+        <h1 id="title">What is Munchkin?</h1>
+        <p>
+          Go down in the dungeon. Kill everything you meet. Backstab your friends and steal their stuff. Grab the treasure and run.
+        </p>
+        <p>
+          Admit it. You love it.
+        </p> 
+        <p>
+          Munchkin is the mega-hit card game about dungeon adventure . . . with none of that stupid roleplaying stuff. You and your friends compete to kill monsters and grab magic items. 
+          And what magic items! Don the Horny Helmet and the Boots of Butt-Kicking. Wield the Staff of Napalm . . . or maybe the Chainsaw of Bloody Dismemberment. 
+          Start by slaughtering the Potted Plant and the Drooling Slime, and work your way up to the Plutonium Dragon . . .
+        </p> 
+        <p>
+          And it's illustrated by John Kovalic!
+        </p>
+        <p>
+          Fast-playing and silly, Munchkin can reduce any roleplaying group to hysteria. And while they're laughing, you can steal their stuff.
+        </p>
+        <p>
+          Ages 10 and up. Plays in one to two hours. Three to six players.
+        </p>
+        <p>
+          Designed by Steve Jackson
+        </p>
+      </div>
+    </div>
     <div id="rest">
     </div>
   </div>
@@ -30,16 +45,21 @@
 
 .contain{
   width : 1300px;
-  height: 580px;
   background-color : white;
-  margin: 20px auto 20px auto;
+  margin: 20px auto;
   text-align: center;
 }
 
 img {
   max-width: 100%;
   height: auto;
-  width: 150px;
+  width: 50%;
+}
+
+.aboutText {
+  width: 60%;
+  margin: 40px auto;
+  text-align: left;
 }
 
 #rest {
